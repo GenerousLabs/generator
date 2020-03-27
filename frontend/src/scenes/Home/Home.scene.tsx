71ac35ac1ba49e00850aa0c257ee6d67faf83c0d
Callum Macdonald <git@callum-macdonald.com>
2020-03-27 11:05:20 +0100

    Experiments with image generation.

--- frontend/src/scenes/Home/Home.scene.tsx
+++ frontend/src/scenes/Home/Home.scene.tsx
@@ -1,9 +1,97 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { fabric } from "fabric";
+
+const width = 300;
+const height = 300;
+
+const lock = {
+  selectable: false
+  // hasControls: false,
+  // lockMovementX: true,
+  // lockMovementY: true,
+  // lockScalingX: true,
+  // lockScalingY: true,
+  // lockUniScaling: true,
+  // lockRotation: true
+};
+
+const isIframeView = () => {
+  return window.location.hash.substr(0, 9) === "#/iframe/";
+};
 
 const Home = () => {
+  const mirror = useRef<HTMLImageElement>(null);
+
+  useEffect(() => {
+    if (isIframeView()) {
+      return;
+    }
+
+    const canvas = new fabric.Canvas("canvas", {});
+
+    const bg = new fabric.Rect({
+      width,
+      height,
+      top: 0,
+      left: 0,
+      fill: "blue",
+      ...lock
+    });
+    canvas.add(bg);
+
+    const firstText = new fabric.Text("Thank you", {
+      top: 40,
+      fontFamily: "sans",
+      fill: "orange",
+      fontSize: 40,
+      ...lock
+    });
+    canvas.add(firstText);
+    firstText.centerH();
+
+    if (mirror && mirror.current && mirror.current.src) {
+      mirror.current.src = canvas.toDataURL();
+      mirror.current.crossOrigin = "anonymous";
+      mirror.current.addEventListener("click", () => {
+        window.location.href = `#/iframe/${canvas.toDataURL()}`;
+        window.location.reload();
+      });
+    }
+  }, []);
+
+  if (isIframeView()) {
+    return (
+      <div>
+        <iframe
+          title="image"
+          src={window.location.hash.substr(9)}
+          frameBorder="0"
+          allowFullScreen
+          style={{ border: 0, width, height }}
+        />
+      </div>
+    );
+  }
+
   return (
     <div>
       <h1>Home</h1>
+      <div id="canvas_container" style={{ position: "relative" }}>
+        <canvas id="canvas" width="300" height="300" />
+        <img
+          id="canvas_mirror"
+          src=""
+          alt="GeneroUS Generator"
+          ref={mirror}
+          style={{
+            width: 300,
+            height: 300,
+            position: "absolute",
+            top: 0,
+            left: 0
+          }}
+        />
+      </div>
     </div>
   );
 };
